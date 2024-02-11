0b7cba3bfb967da26e145359288395d85cf9e5b7
djedd1ne <58569774+djedd1ne@users.noreply.github.com>
2024-02-11 06:07:18 +0100

    add integer_overflow example

---
+++ 3_integer_overflow.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+
+#include <iostream>
+
+int main() {
+    int x = 2147483647; // max value for a signed 32-bit integer
+    int y = 1; // assign 1 to y
+    int z = x + y; // x and y addition
+    cout << "The value of z is: " << z << endl; // Print z value
+}
